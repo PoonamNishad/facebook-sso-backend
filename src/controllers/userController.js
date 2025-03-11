06fca06dbc749860b42ed9887b22b74c798aca50
poonam <72191865+PoonamNishad@users.noreply.github.com>
2025-03-11 17:25:08 +0530

    changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 const axios = require("axios");
 
 exports.getProfile = (req, res) => {
-
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
     return res.status(401).json({ error: "Unauthorized: No token provided" });
@@ -16,7 +15,6 @@ exports.getProfile = (req, res) => {
   }
 };
 
-
 exports.getUserPages = async (req, res) => {
   const userAccessToken = req.headers.authorization?.split(" ")[1];
 
@@ -24,9 +22,12 @@ exports.getUserPages = async (req, res) => {
     return res.status(401).json({ error: "Unauthorized" });
   }
   try {
-    const response = await axios.get("https://graph.facebook.com/v22.0/me/accounts?type=page&", {
-      params: { access_token: userAccessToken },
-    });
+    const response = await axios.get(
+      "https://graph.facebook.com/v22.0/me/accounts?type=page&",
+      {
+        params: { access_token: userAccessToken },
+      }
+    );
     res.json(response.data);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch pages", error: error });
@@ -38,21 +39,27 @@ exports.getPageInsights = async (req, res) => {
 
   const { pageId } = req.query;
 
-  if (!pageId || !userAccessToken) {
-    return res.status(400).json({ error: "Page ID and Access Token are required" });
+  if (!pageId || !userAccessToken || pageId == "null") {
+    console.log(pageId, "pageId");
+    return res
+      .status(400)
+      .json({ error: "Page ID and Access Token are required" });
   }
 
   try {
-    const response = await axios.get(`https://graph.facebook.com/v17.0/${pageId}/insights`, {
-      params: {
-        metric: "page_fan_count,page_engaged_users,page_impressions,page_actions_post_reactions_total",
-        access_token: userAccessToken,
-      },
-    });
+    const response = await axios.get(
+      `https://graph.facebook.com/v17.0/${pageId}/insights`,
+      {
+        params: {
+          metric:
+            "page_fan_count,page_engaged_users,page_impressions,page_actions_post_reactions_total",
+          access_token: userAccessToken,
+        },
+      }
+    );
 
     res.json(response.data);
   } catch (error) {
-    console.error("Error fetching page insights:", error.response?.data || error);
     res.status(500).json({ error: "Failed to fetch page insights" });
   }
 };
